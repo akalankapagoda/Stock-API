5676b53f42015ec8f9cd09eca478a0684e94782e
akalankapagoda <akalankapagoda@gmail.com>
2022-06-17 22:31:48 +0530

    Initial commit of Spring Initializer Output

---
+++ src/main/java/com/payconiq/stock/stocksapi/StocksApiApplication.java
@@ -0,0 +1,13 @@
+package com.payconiq.stock.stocksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payconiq/stock/stocksapi/StocksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payconiq.stock.stocksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
