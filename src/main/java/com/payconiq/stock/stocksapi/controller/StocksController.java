1bd04f81dcce8f0370e1c7f17648cab89ca906cf
akalankapagoda <akalankapagoda@gmail.com>
2022-06-26 19:20:27 +0530

    Add Swagger UI screenshot and API descriptions.

--- src/main/java/com/payconiq/stock/stocksapi/controller/StocksController.java
+++ src/main/java/com/payconiq/stock/stocksapi/controller/StocksController.java
@@ -5,6 +5,7 @@ import com.payconiq.stock.stocksapi.model.ListStocksResponse;
 import com.payconiq.stock.stocksapi.model.Stock;
 import com.payconiq.stock.stocksapi.model.UpdateStockRequest;
 import com.payconiq.stock.stocksapi.service.StocksService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,7 @@ public class StocksController {
      * @return A simple hello message
      */
     @GetMapping("/hello")
+    @Tag(name = "Health Check", description = "Endpoints for checking server health")
     public String getHello() {
         return "Hello";
     }
@@ -41,6 +43,7 @@ public class StocksController {
      * @return List of stocks for the provided page and pagination information
      */
     @GetMapping
+    @Tag(name = "List Stocks", description = "Lists stocks with pagination")
     public ListStocksResponse listStocks(@RequestParam(name = "pageNo", defaultValue = "0") Integer pageNo,
                                          @RequestParam(name = "pageSize", defaultValue = "10") Integer pageSize) {
 
@@ -55,6 +58,7 @@ public class StocksController {
      * @return The stock
      */
     @GetMapping("{id}")
+    @Tag(name = "Get Stock", description = "Retrieves a specific stock")
     public Stock getStock(@PathVariable Integer id) {
 
 
@@ -75,6 +79,7 @@ public class StocksController {
      * @return Created Stock
      */
     @PostMapping
+    @Tag(name = "Create Stocks", description = "Creates a new stock")
     public Stock createStock(@RequestBody CreateStockRequest createStockRequest) {
 
         return stocksService.createStock(createStockRequest.getName(), createStockRequest.getCurrentPrice());
@@ -88,6 +93,7 @@ public class StocksController {
      * @return Updated stock
      */
     @PatchMapping("{id}")
+    @Tag(name = "Update Stock", description = "Updates the price of an existing stock")
     public Stock updateStockPrice(@PathVariable Integer id, @RequestBody UpdateStockRequest updateStockRequest) {
 
         return stocksService.updateStock(id, updateStockRequest.getCurrentPrice());
@@ -102,6 +108,7 @@ public class StocksController {
      * @return Deleted stock
      */
     @DeleteMapping("{id}")
+    @Tag(name = "Delete Stock", description = "Deletes and existing stock price")
     public Stock deleteStock(@PathVariable Integer id) {
 
         return stocksService.deleteStock(id);
